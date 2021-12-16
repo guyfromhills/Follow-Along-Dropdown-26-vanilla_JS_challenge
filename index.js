e067367a0e8b366705b3d82db52fd8681948442b
Anubhav Negi <anubhavnegi54@gmail.com>
2021-12-16 22:36:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,72 +4,86 @@
 
 
 
-// grabbing trigger
-const triggers = document.querySelectorAll(".cool > li");
 
-//grab dropdown background
+//grab triggers
+ const triggers = document.querySelectorAll(".cool > li");
+
+//grab background 
 const background = document.querySelector(".dropdownBackground");
 
 //grab nav
 const nav = document.querySelector(".top");
 
-function handleEnter()
+
+//funct for mouse enters
+function handleMouseEnter(e)
 {
 
+    //adding trigger-enter class to all triggers, to get dropdown display : block
     this.classList.add("trigger-enter");
 
-    setTimeout(()=> {
-        if( this.classList.contains("trigger-enter")){
-            this.classList.add("trigger-enter-active")
-        }
-    }, 150);
+    //adding trigger-enter-active class to all triggers, to get dropdown visible
+    setTimeout(  () => {this.classList.add("trigger-enter-active") }, 200);
 
-
-
-
+    //add open class to background
     background.classList.add("open");
 
+    //grab dropdown
     const dropdown = this.querySelector(".dropdown");
 
-    const dropdownCoords = dropdown.getBoundingClientRect();
+    //get DOMRect of dropdown
+    const coordsDropdown = dropdown.getBoundingClientRect();
 
+    //get DOMRect of nav
     const navCoords = nav.getBoundingClientRect();
-    console.log(navCoords);
 
-    const coords = { 
-        height: dropdownCoords.height,
-        width: dropdownCoords.width,
-        top: dropdownCoords.top - navCoords.top,
-        left: dropdownCoords.left - navCoords.left
-    
+    //creating coordinates
+    coords = { 
+        height: coordsDropdown.height,
+        width: coordsDropdown.width,
+        top: coordsDropdown.top - navCoords.top,
+        left: coordsDropdown.left - navCoords.left
+    }
 
-    };
-
+    //set width of background
     background.style.setProperty("width", `${coords.width}px`);
+
+    //set height of background
     background.style.setProperty("height", `${coords.height}px`);
-    background.style.setProperty("transform", `translate(${coords.left}px, ${coords.top}px)`);
+
+    //set transform of background
+    background.style.setProperty("transform", `translate(${coords.left}px, ${coords.top}px`);
+
 
 
   
+
 }
 
-function handleLeave()
+//funct for mouse enters
+function handleMouseExit()
 {
 
+    //removing trigger-enter and trigger-enter-active classes on mouse hover exit
+    this.classList.remove("trigger-enter","trigger-enter-active");
 
-    this.classList.remove("trigger-enter", "trigger-enter-active");
+    //remove open class to background
     background.classList.remove("open");
 
 
+  
 
 
 }
 
-triggers.forEach( function (trigger){
-    trigger.addEventListener("mouseenter", handleEnter);
+
+//if mouse hovers over triggers, call funn handleMouseEnter
+triggers.forEach( function ( trigger){
+    trigger.addEventListener("mouseenter", handleMouseEnter);
 })
 
 
-triggers.forEach( function (trigger){
-    trigger.addEventListener("mouseleave", handleLeave);
+//if mouse leaves triggers, call funn handleMouseExit
+triggers.forEach( function ( trigger){
+    trigger.addEventListener("mouseleave", handleMouseExit);
 })
